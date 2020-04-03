2cb30babd34bba827526a5cbc0207486eb7ba657
Shambhavi Nandan <shambhavi.physics@gmail.com>
2020-04-03 16:51:41 +0200

    covid diffusion simulation

--- Covid_Simulation2D.py
+++ Covid_Simulation2D.py
@@ -1,4 +1,3 @@
-
 # Author: Shambhavi Nandan
 
 # This code implements 2D diffusion model for pandemic spread. The original paper can be found her: https://api.semanticscholar.org/CorpusID:9108055.oa
@@ -19,9 +18,9 @@ T = lambda index: (index[0] - 1, index[1])
 B = lambda index: (index[0] + 1, index[1])
 
 def assemble_a_to_A(rows, columns, aPs, aEs, aWs, aNs, aSs):
-
+    
     A = np.zeros((rows * columns, rows *  columns))
-
+    
     row = 0
     for index in np.ndindex(rows, columns):
         
@@ -135,16 +134,16 @@ def assemble_a_to_A(rows, columns, aPs, aEs, aWs, aNs, aSs):
             k = i_j_to_k(S_index, columns)
             y[k] = -aSs[index]
         
-        A[row, :] = y[:]
+        A[row, :] = y[:] 
         
         row += 1
-
+    
     return A
 
 # Maine code implementing the model starts from here:
 
 def solve_Succeptibility(Ds,Ic_old,Sc_old,dt,nx,ny):
-
+    
     # Succeptibility datastructure:
     ac = np.zeros((ny,nx))
     al = np.zeros((ny,nx))
@@ -232,109 +231,130 @@ def solve_Succeptibility(Ds,Ic_old,Sc_old,dt,nx,ny):
                     at[j,i] = Ds/dx**2
                     ab[j,i] = Ds/dx**2
                     source[j,i] = Sc_old[j,i]/dt
-
+    
     A = assemble_a_to_A(ny-2, nx-2, ac[1:-1,1:-1],ar[1:-1,1:-1],al[1:-1,1:-1],at[1:-1,1:-1],                        ab[1:-1,1:-1])
     sol = np.linalg.solve(A,source[1:-1,1:-1].flatten())
-
+    
+    sol = np.reshape(sol,(ny-2,nx-2))
+    
     return sol
 
 def solve_Infection(Di,Ic_old,Sc_old,dt,nx,ny):
+    
+    n_it = 0 # iterator
 
-    # Infection datastructure:
-    ac = np.zeros((ny,nx))
-    al = np.zeros((ny,nx))
-    ar = np.zeros((ny,nx))
-    at = np.zeros((ny,nx))
-    ab = np.zeros((ny,nx))
-    source = np.zeros((ny,nx))
-    jmin = 1
-    jmax = ny-2
-    for j in np.arange(jmin, jmax+1):
-        if j==jmin: # top row
-            imin = 1
-            imax = nx-2
-            for i in np.arange(imin, imax+1):
-                if i==1: # left corner node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = 0.
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = 0.
-                    ab[j,i] = Ds/dx**2
-                    source[i,j] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i>imin and i<imax: # boundary nodes:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = 0.
-                    ab[j,i] = Ds/dx**2
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i==imax: # right corner node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = 0.
-                    at[j,i] = 0.
-                    ab[j,i] = Ds/dx**2
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-
-        elif j==jmax: # bottom row
-            imin = 1
-            imax = nx-2
-            for i in np.arange(imin, imax+1):
-                if i==imin: # left corner node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = 0.
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = 0.
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i>imin and i<imax: # boundary nodes:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = 0.
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i==imax: # right corner node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = 0.
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = 0.
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-
-        else: # internal rows
-            imin = 1
-            imax = nx-2
-            for i in np.arange(imin, imax+1):
-                if i==imin: # left boundary node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = 0.
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = Ds/dx**2
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i>imin and i<imax: # internal nodes:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = Ds/dx**2
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = Ds/dx**2
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-                elif i==imax: # right boundary node:
-                    ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
-                    al[j,i] = Ds/dx**2
-                    ar[j,i] = 0.
-                    at[j,i] = Ds/dx**2
-                    ab[j,i] = Ds/dx**2
-                    source[j,i] = Ic_old[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
-
-    A = assemble_a_to_A(ny-2, nx-2, ac[1:-1,1:-1],ar[1:-1,1:-1],al[1:-1,1:-1],at[1:-1,1:-1],                        ab[1:-1,1:-1])
-    sol = np.linalg.solve(A,source[1:-1,1:-1].flatten())
-
+    e = 1.0e-3 # tolerance for convergence
+    
+    Ic_guess = np.zeros((ny,nx))
+    Ic_guess[:,:] = Ic_old[:,:]
+    
+    while True:
+        n_it += 1
+        # Infection datastructure:
+        ac = np.zeros((ny,nx))
+        al = np.zeros((ny,nx))
+        ar = np.zeros((ny,nx))
+        at = np.zeros((ny,nx))
+        ab = np.zeros((ny,nx))
+        source = np.zeros((ny,nx))
+        jmin = 1
+        jmax = ny-2
+        for j in np.arange(jmin, jmax+1):
+            if j==jmin: # top row
+                imin = 1
+                imax = nx-2
+                for i in np.arange(imin, imax+1):
+                    if i==1: # left corner node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = 0.
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = 0.
+                        ab[j,i] = Ds/dx**2
+                        source[i,j] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+                    elif i>imin and i<imax: # boundary nodes:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = 0.
+                        ab[j,i] = Ds/dx**2
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+                    elif i==imax: # right corner node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = 0.
+                        at[j,i] = 0.
+                        ab[j,i] = Ds/dx**2
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+    
+            elif j==jmax: # bottom row
+                imin = 1
+                imax = nx-2
+                for i in np.arange(imin, imax+1):
+                    if i==imin: # left corner node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = 0.
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = 0.
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+                    elif i>imin and i<imax: # boundary nodes:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = 0.
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+                    elif i==imax: # right corner node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = 0.
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = 0.
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+    
+            else: # internal rows
+                imin = 1
+                imax = nx-2
+                for i in np.arange(imin, imax+1):
+                    if i==imin: # left boundary node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = 0.
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = Ds/dx**2
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+                    elif i>imin and i<imax: # internal nodes:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = Ds/dx**2
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = Ds/dx**2
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt 
+                    elif i==imax: # right boundary node:
+                        ac[j,i] = (1./dt) + 1. + 4*Ds/dx**2
+                        al[j,i] = Ds/dx**2
+                        ar[j,i] = 0.
+                        at[j,i] = Ds/dx**2
+                        ab[j,i] = Ds/dx**2
+                        source[j,i] = Ic_guess[j,i]*Sc_old[j,i] + Ic_old[j,i]/dt
+        
+        A = assemble_a_to_A(ny-2, nx-2, ac[1:-1,1:-1],ar[1:-1,1:-1],al[1:-1,1:-1],at[1:-1,1:-1],                        ab[1:-1,1:-1])
+        sol = np.linalg.solve(A,source[1:-1,1:-1].flatten())
+        
+        sol = np.reshape(sol,(ny-2,nx-2))
+        
+        print(np.sum(np.abs(Ic_guess[1:-1,1:-1]-sol)),'\n')
+        
+        if np.all(np.abs(Ic_guess[1:-1,1:-1]-sol) <= e) or n_it == 100:
+            print('####### Convergence for Infection ########','\n')
+            break
+        else:
+            Ic_guess[1:-1,1:-1] = sol[:,:]
+    
     return sol
 
 def solve_Recovery(Ic_old,Rc_old,dt,nx,ny):
-
+    
     # Infection datastructure:
     ac = np.zeros((ny,nx))
     al = np.zeros((ny,nx))
@@ -422,18 +442,20 @@ def solve_Recovery(Ic_old,Rc_old,dt,nx,ny):
                     at[j,i] = 0.
                     ab[j,i] = 0.
                     source[i,j] = Ic_old[j,i] + Rc_old[j,i]/dt
-
+    
     A = assemble_a_to_A(ny-2, nx-2, ac[1:-1,1:-1],ar[1:-1,1:-1],al[1:-1,1:-1],at[1:-1,1:-1],                        ab[1:-1,1:-1])
     sol = np.linalg.solve(A,source[1:-1,1:-1].flatten())
-
+    
+    sol = np.reshape(sol,(ny-2,nx-2))
+    
     return sol
+
 
 # Grid parameters:
 nx = 40
 ny = nx
 lx = 100
 ly = lx
-
 dx = lx / nx
 dy = ly / ny
 
@@ -445,6 +467,7 @@ S[2,3] = 6.
 S[3,4] = 5.
 S[2,4] = 6.
 #S[1:-1,1:-1] = np.random.uniform(low=0.0, high=1.0, size=(ny-2,nx-2))
+
 # Infection initialization:
 I = np.zeros((ny, nx))
 I[3,4] = 1.
@@ -453,31 +476,28 @@ I[3,4] = 1.
 Rc = np.zeros((ny, nx))
 Rc[1:-1,1:-1] = 0.0
 
-S_old = np.zeros((ny, nx))
-I_old = np.zeros((ny, nx))
-R_old = np.zeros((ny, nx))
-
-# Physical parameters:
+# Parameters:
 Ds = 1.0e-1
 Di = 1.0e-1
 dt = 1.0e-1
 
-
-
 # Time parameters:
+S_old = np.zeros((ny, nx))
+I_old = np.zeros((ny, nx))
+R_old = np.zeros((ny, nx))
 nt = 10.
-
 t = 0.
+
 while t < nt:
     S_old[1:-1,1:-1] = S[1:-1,1:-1]
     I_old[1:-1,1:-1] = I[1:-1,1:-1]
     R_old[1:-1,1:-1] = Rc[1:-1,1:-1]
     I_sol = solve_Infection(Di,I_old,S_old,dt,nx,ny)
     S_sol = solve_Succeptibility(Ds,I_old,S_old,dt,nx,ny)
-    R_sol = solve_Recovery(I_old,R_old,dt,nx,ny)
-    S[1:-1,1:-1] = np.reshape(S_sol,(ny-2,nx-2))
-    I[1:-1,1:-1] = np.reshape(I_sol,(ny-2,nx-2))
-    Rc[1:-1,1:-1] = np.reshape(R_sol,(ny-2,nx-2))
+    #R_sol = solve_Recovery(I_old,R_old,dt,nx,ny)
+    S[1:-1,1:-1] = S_sol
+    I[1:-1,1:-1] = I_sol
+    #Rc[1:-1,1:-1] = R_sol
 
     t = t+dt
 
